2142d34ca087ae6e4bb41542a63f214e91c9b95b
smileBaxin@163.com <>
2021-02-02 14:19:15 +0800

    add first function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-export const consoleName = ()=>{
-    console.log('ywxds')
+import moment from 'moment'
+import _ from 'lodash'
+
+export const dateFormat = (string, startFormat, endFormat) => {
+    return moment(string, _.castArray(startFormat)).format(endFormat)
 }
